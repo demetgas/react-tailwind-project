0772c6371b48acfa1d3265e96833a5658b885afe
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 12:03:27 +0200

    added icon

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-700 h-16">
       <div className="pl-16 text-2xl text-gray-400 hover:text-3xl transition-all cursor-pointer">
         CATS
+        <FontAwesomeIcon className="icon" icon={faCat} />
       </div>
       <div className="flex space-x-6 pr-16 text-gray-400 text-xl cursor-pointer">
         <div className="hover:text-white">LILI</div>
