3b6f59f7879d26b5f3cb6b2984568f50af87d838
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 14:36:33 +0200

    a

--- tailwind-app/src/components/Hero.jsx
+++ tailwind-app/src/components/Hero.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <div className="w-full h-[90vh]">
+    <div className="relative w-full h-[90vh] flex justify-end">
       <img
-        src="https://images2.alphacoders.com/133/1335809.png"
+        src="https://mrwallpaper.com/images/hd/minimalist-black-cat-backgrounds-3gy1fpvdc0tbdimn.jpg"
         alt="/"
         className="w-full h-full object-cover transform scale-x-[-1]"
       />
-      <div className="max-w-[1140px] m-auto">
-        <div className="absolute top-[40%]   flex flex-col text-white p-4">
+      <div className="max-w-[1140px] m-auto absolute top-[35%] left-80">
+        <div className="text-white p-4">
           <h1 className="text-4xl">Welcome to the Cat Paradise</h1>
-          <h2 className="text-4xl py-4 italic">Meet Our Furry Friends</h2>
+          <h2 className="text-3xl py-4 italic">Meet Our Furry Friends...</h2>
           <p className="text-justify max-w-md">
             Here at our home, we are proud to introduce you to our beloved
             feline companions. Each one has their own unique personality and
@@ -19,6 +19,11 @@ export const Hero = () => {
             journey as we share tales of mischief, cuddles, and everything in
             between!
           </p>
+        </div>
+      </div>
+      <div className="max-w-[1140px] m-auto absolute top-[35%] right-80">
+        <div>
+          <img src="" alt="" />
         </div>
       </div>
     </div>
