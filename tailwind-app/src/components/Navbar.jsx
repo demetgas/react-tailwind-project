c25875a9865cfb110208465f0c177ae00e08b1ed
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 11:39:17 +0200

    navbar

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="bg-grey-200">
-      <div className="text-black-500">Navbar</div>
+    <div className="flex justify-between items-center bg-gray-700 h-16">
+      <div className="pl-16 text-2xl text-gray-400">CATS</div>
+      <div className="flex space-x-6 pr-16 text-gray-400 text-xl">
+        <div className="hover:text-white">LILI</div>
+        <div className="hover:text-white">NINA</div>
+        <div className="hover:text-white">MALKOC</div>
+        <div className="hover:text-white">FREYJA</div>
+      </div>
     </div>
   );
 };
