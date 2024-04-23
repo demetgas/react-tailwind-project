18a71be5aebe777dd07784437f3b4f8d309f409b
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 16:04:42 +0200

    Update Cats.jsx

--- tailwind-app/src/components/Cats.jsx
+++ tailwind-app/src/components/Cats.jsx
@@ -5,12 +5,15 @@ export const Cats = () => {
   return (
     <div className="max-w-[1140px] m-auto w-full md:flex mt-[-75px]">
       {cats.map((cat) => (
-        <div className="relative p-4" key={cat.id}>
+        <div
+          className="relative p-4 hover:scale-110 delay-75 cursor-pointer"
+          key={cat.id}
+        >
           <h3 className="absolute z-10 top-[80%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 bg-black text-white bg-opacity-70 rounded-md p-1.5 border-black text-xl font-bold">
             {cat.name}
           </h3>
           <img
-            className="w-64 h-64 object-cover relative border-4 border-black p-2 shadow-lg hover:scale-110 delay-75 cursor-pointer"
+            className="w-64 h-64 object-cover relative border-4 border-black p-2 shadow-lg "
             src={cat.img}
             alt="/"
           />
