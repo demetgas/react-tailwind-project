d1472fc8d04d8a22b2de7020299550e2c87276f1
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 11:15:29 +0200

    created navbar

--- tailwind-app/src/App.js
+++ tailwind-app/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 class="text-6xl text-red-500 bg-purple-200">Helloss</h1>
+        <Navbar />
       </header>
     </div>
   );
---
+++ tailwind-app/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="bg-grey-200">
+      <div className="text-black-500">Navbar</div>
+    </div>
+  );
+};
