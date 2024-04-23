14967cf1cb6185b7f1805dad2e7554a57c5c491c
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 15:02:22 +0200

    created cats

--- tailwind-app/src/App.js
+++ tailwind-app/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Cats } from "./components/Cats";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <Navbar />
         <Hero />
+        <Cats />
       </header>
     </div>
   );
