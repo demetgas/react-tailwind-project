87fa292835fb1bdf7a75cd0dea6de9ac77d940fe
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-23 11:43:48 +0200

    Update Navbar.jsx

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-700 h-16">
-      <div className="pl-16 text-2xl text-gray-400">CATS</div>
+      <div className="pl-16 text-2xl text-gray-400 hover:text-3xl">CATS</div>
       <div className="flex space-x-6 pr-16 text-gray-400 text-xl">
         <div className="hover:text-white">LILI</div>
         <div className="hover:text-white">NINA</div>
