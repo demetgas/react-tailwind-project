8ad9c2ad98c096dd07e97498c1f5d1a4535ab9dd
Demet Gaş <demet_gas@hotmail.com>
2024-04-24 23:02:29 +0200

    Update Gallery.jsx

--- tailwind-app/src/components/Gallery.jsx
+++ tailwind-app/src/components/Gallery.jsx
@@ -4,19 +4,19 @@ import galleryArray from "../data/gallery";
 export const Gallery = () => {
   return (
     <div>
-      <h2 className="text-center text-gray-700 mt-[480px] font-bold text-4xl ">
+      <h2 className="text-center text-gray-700 mt-[480px] font-bold text-4xl">
         Gallery
       </h2>
-      <div className="grid grid-cols-2  max-w-[90rem] gap-20">
+      <div className="grid grid-cols-2 max-w-[90rem]">
         {galleryArray.map((array) => (
-          <div className="w-fit h-[65vh] mt-20  mx-auto">
-            <div className="grid grid-cols-4  ">
+          <div className="w-fit h-[65vh] mt-20 mx-auto mx-2">
+            <div className="grid grid-cols-4 gap-4">  
               {array.map((picture) => (
                 <img
                   key={picture.id}
                   src={picture.img}
-                  alt=""
-                  className="max-w-60 max-h-60 "
+                  alt=''  
+                  className="max-w-60 max-h-60  my-2"  
                 />
               ))}
             </div>
