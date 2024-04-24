eb64d9741b3a33cb34268c1de3289bbec11fd0e4
Demet Gaş <demet_gas@hotmail.com>
2024-04-24 23:15:14 +0200

    Update Message.jsx

--- tailwind-app/src/components/Message.jsx
+++ tailwind-app/src/components/Message.jsx
@@ -9,33 +9,33 @@ export const Message = () => {
       <p className="text-center text-gray-700 text-2xl">
         Can't wait to hear your thoughts!
       </p>
-      <div className="grid md:grid-cols-2 mt-16 mx-20 ">
+      <div className="grid md:grid-cols-2 mt-16 mx-20">
         <img
           src={cat}
           alt="/"
           className=" md:h-full  p-2 max-h-[500px] h-[200px] w-[420px]"
         />
         <form>
-          <div className="grid grid-cols-2 ">
-            <input className="border m-2 p-2" type="text" placeholder="Name" />
+          <div className="grid grid-cols-2  ">
+            <input className="border m-2 p-2 rounded" type="text" placeholder="Name" />
             <input
-              className="border m-2 p-2"
+              className="border m-2 p-2 rounded"
               type="text"
               placeholder="Last Name"
             />
             <input
-              className="border m-2 p-2"
+              className="border m-2 p-2 rounded"
               type="email"
               placeholder="Email"
             />
-            <input className="border m-2 p-2" type="tel" placeholder="Phone" />
+            <input className="border m-2 p-2 rounded" type="tel" placeholder="Phone" />
             <input
-              className="border col-span-2 p-2 m-2"
+              className="border col-span-2 p-2 m-2 rounded"
               type="text"
               placeholder="Address"
             />
             <textarea
-              className="border col-span-2 m-2 p-2"
+              className="border col-span-2 m-2 p-2 rounded"
               cols="30"
               rows="10"
             ></textarea>
