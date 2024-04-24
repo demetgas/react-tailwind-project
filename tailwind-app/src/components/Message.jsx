0cc40bd72bac905d59f1b891ff083874d912dfac
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-24 13:44:37 +0200

    Update Message.jsx

--- tailwind-app/src/components/Message.jsx
+++ tailwind-app/src/components/Message.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 export const Message = () => {
   return (
-    <div className="w-full h-[60vh]">
+    <div className="w-full h-[60vh] mt-60">
       <div>
-        <div>Send us a message</div>
-        <div>Can't wait to hear your thoughts!</div>
+        <div className="text-center text-gray-700 p-4 font-bold text-4xl">
+          Send us a message
+        </div>
+        <div className="text-center text-gray-700 p-4 font-bold text-2xl">
+          Can't wait to hear your thoughts!
+        </div>
         <img src="" alt="" />
       </div>
     </div>
