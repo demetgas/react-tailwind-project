9d4fc605c9d313674f76921468997742892dd688
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-24 11:15:48 +0200

    Update Gallery.jsx

--- tailwind-app/src/components/Gallery.jsx
+++ tailwind-app/src/components/Gallery.jsx
@@ -3,8 +3,8 @@ import gallery from "../data/gallery";
 
 export const Gallery = () => {
   return (
-    <div className="w-full h-[70vh]">
-      <div className="flex w-70 h-60 mt-40">
+    <div className="w-full h-[70vh] mt-60">
+      <div className="flex w-70 h-60">
         {" "}
         {gallery.map((picture) => (
           <img src={picture.img} alt="" />
