a3d7264a4ba4f59daf56b445562c4f890708203c
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-24 10:56:36 +0200

    added pictures

--- tailwind-app/src/components/Gallery.jsx
+++ tailwind-app/src/components/Gallery.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import gallery from "../data/gallery";
 
 export const Gallery = () => {
   return (
     <div className="w-full h-[70vh]">
-      <div className="bg-black mt-60">h</div>
+      <div className="flex w-70 h-60 mt-40">
+        {" "}
+        {gallery.map((picture) => (
+          <img src={picture.img} alt="" />
+        ))}{" "}
+      </div>
     </div>
   );
 };
