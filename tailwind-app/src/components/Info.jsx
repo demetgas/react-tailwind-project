decc2fc64e4c6951c5476ff0ffc67c23cb3114e0
Demet Gaş <demet_gas@hotmail.com>
2024-04-24 22:38:26 +0200

    Update Info.jsx

--- tailwind-app/src/components/Info.jsx
+++ tailwind-app/src/components/Info.jsx
@@ -2,19 +2,18 @@ import React from "react";
 
 export const Info = () => {
   return (
-    <div className=" w-full mt-[270px] h-[45svh] bg-babyblue">
-      <div className="grid grid-cols-2 gap-32 max-w-[1140px] m-auto">
+    <div className="w-full mt-[270px] h-auto bg-babyblue">
+      <div className="grid grid-cols-2 gap-32 max-w-[1140px] mx-auto py-10">
         <div className="text-justify">
           <h2 className="text-center text-gray-700 p-4 font-bold text-4xl italic">
             Cats are fascinating creatures
           </h2>
-          <p className="mb-7 mt-10 text-lg ">
-            {" "}
+          <p className="mb-7 mt-10 text-lg">
             Cats' unique behaviors, from their playful antics to their grooming
             rituals, highlight their captivating nature. Despite their
             independent streak, cats form strong bonds with their human
             companions, offering companionship and warmth in a way that's truly
-            special.{" "}
+            special.
           </p>
           <p className="mb-10 text-lg">
             One of the most captivating aspects of cats is their remarkable
@@ -25,16 +24,18 @@ export const Info = () => {
             skilled hunters and agile explorers of their environments.
           </p>
           <p className="text-lg">
-            {" "}
             Understanding and appreciating the intricacies of cat behavior not
             only deepens our connection with these remarkable animals but also
             enriches our own lives in immeasurable ways.
           </p>
         </div>
-        <img
-          src="https://images.vexels.com/media/users/3/272056/isolated/preview/54b293603423b19ebd21fee4c7f475e8-cute-black-cartoon-cat.png"
-          alt=""
-        />
+        <div className="flex justify-center items-center">
+          <img
+            src="https://images.vexels.com/media/users/3/272056/isolated/preview/54b293603423b19ebd21fee4c7f475e8-cute-black-cartoon-cat.png"
+            alt=""
+            className="max-w-[400px] max-h-full" 
+          />
+        </div>
       </div>
     </div>
   );
