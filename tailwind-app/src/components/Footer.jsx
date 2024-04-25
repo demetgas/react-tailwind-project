04af6f0eaa2f63ce64a78b6dc2d9242dfd23b940
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-25 14:46:15 +0200

    made footer responsive

--- tailwind-app/src/components/Footer.jsx
+++ tailwind-app/src/components/Footer.jsx
@@ -2,82 +2,79 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="w-full h-max-[45svh] bg-hero mt-[280px] flex text-xl justify-center gap-8">
-      <div className="text-white flex flex-col ">
-        <h2 className="mt-[90px]  text-2xl">About Us</h2>
-        <a className="my-[10px]" href="/">
+    <div className="w-full bg-hero mt-[280px] flex flex-wrap justify-center gap-8 text-white text-xl">
+      <div className="flex flex-col mb-8 my-10">
+        <h2 className="text-2xl mb-4">About Us</h2>
+        <a className="my-2" href="/">
           How it works
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Testimonials
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Careers
         </a>
-        <a className="my-[10px]" href="/">
-          Investments
-        </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Terms of Service
         </a>
       </div>
-      <div className="text-white flex flex-col ml-[80px]">
-        <h2 className="mt-[90px]  text-2xl">Contact Us</h2>
-        <a className="my-[10px]" href="/">
+      <div className="flex flex-col mb-8">
+        <h2 className="text-2xl mb-4 my-10">Contact Us</h2>
+        <a className="my-2" href="/">
           Contact
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Support
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Destinations
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Sponsorships
         </a>
-      </div>{" "}
-      <div className="text-white flex flex-col ml-[80px]">
-        <h2 className="mt-[90px]  text-2xl">Videos</h2>
-        <a className="my-[10px]" href="/">
+      </div>
+      <div className="flex flex-col mb-8">
+        <h2 className="text-2xl mb-4 my-10 ">Videos</h2>
+        <a className="my-2" href="/">
           Submit Video
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Ambassadors
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Agency
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Influencer
         </a>
-      </div>{" "}
-      <div className="text-white flex flex-col ml-[80px]">
-        <h2 className="mt-[90px]  text-2xl">Social Media</h2>
-        <a className="my-[10px]" href="/">
+      </div>
+      <div className="flex flex-col mb-8">
+        <h2 className="text-2xl mb-4 my-10">Social Media</h2>
+        <a className="my-2" href="/">
           Instagram
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Facebook
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Youtube
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Twitter
         </a>
-      </div>{" "}
-      <div className="text-white flex flex-col ml-[80px]">
-        <h2 className="mt-[90px] text-2xl">Little Paws</h2>
-        <a className="my-[10px]" href="/">
+      </div>
+      <div className="flex flex-col mb-8">
+        <h2 className="text-2xl mb-4 my-10">Little Paws</h2>
+        <a className="my-2" href="/">
           Lili
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Nina
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Malkoç
         </a>
-        <a className="my-[10px]" href="/">
+        <a className="my-2" href="/">
           Freyja
         </a>
       </div>
