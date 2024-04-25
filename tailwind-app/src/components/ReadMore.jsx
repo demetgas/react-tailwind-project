c37ab18b627cf2d1f93303f901ae8a338faaed8e
Demet Gaş <demet_gas@hotmail.com>
2024-04-25 20:38:21 +0200

    Update ReadMore.jsx

--- tailwind-app/src/components/ReadMore.jsx
+++ tailwind-app/src/components/ReadMore.jsx
@@ -8,7 +8,7 @@ export const ReadMore = () => {
       <img
         src="https://m.media-amazon.com/images/M/MV5BOWI3NzQ0MTAtMGQzNi00NjY5LTk5YTYtZjE0M2EzODlkNGRhXkEyXkFqcGdeQXVyMjAwNzczNTU@._V1_.jpg"
         alt=""
-        className="lg:max-w-[16svw] lg:max-h-[45svh] md:w-[45svw] md:h-[40svh] shadow-2xl shadow-hero hover:scale-110 transition-all delay-75"
+        className="lg:max-w-[18svw] lg:max-h-[45svh] md:w-[45svw] md:h-[40svh] shadow-2xl shadow-hero hover:scale-110 transition-all delay-75"
       />
       <div className="flex flex-col lg:mt-0 md:mt-20">
         <p className="underline lg:text-[20px] md:text-[17px] font-bold cursor-pointer">
