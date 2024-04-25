c85550152fa313e92915285e9336c61c3a8e56d1
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-25 16:10:26 +0200

    responsive

--- tailwind-app/src/components/Gallery.jsx
+++ tailwind-app/src/components/Gallery.jsx
@@ -3,11 +3,11 @@ import galleryArray from "../data/gallery";
 
 export const Gallery = () => {
   return (
-    <div className="w-full h-[80svh] mx-auto">
+    <div className="w-full md:h-[230svh] lg:h-[80svh] mx-auto">
       <h2 className="text-center text-gray-700 mt-[280px] font-bold text-4xl">
         The pictures sent by our users...
       </h2>
-      <div className="flex justify-center gap-20">
+      <div className="flex sm:flex-col md:flex-col lg:flex-row justify-center gap-20">
         <div className="border-x-8 border-y-8 border-babyblue my-auto max-h-[45svh] ">
           <h1 className="text-center text-gray-700 text-[25px] mt-12  ">
             Thank you
@@ -35,16 +35,16 @@ export const Gallery = () => {
             and meaningful. Thank you for being a part of our community!
           </p>
         </div>
-        <div className="grid grid-cols-2 max-w-[90rem]  ">
+        <div className="grid md:grid-cols-1  lg:grid-cols-2 lg:max-w-[90rem]  ">
           {galleryArray.map((array) => (
-            <div className=" h-[65vh] mt-20  ">
-              <div className="grid grid-cols-4">
+            <div className="  mt-20  ">
+              <div className="grid lg:grid-cols-4 md:grid-cols-2">
                 {array.map((picture) => (
                   <img
                     key={picture.id}
                     src={picture.img}
                     alt=""
-                    className="max-w-60 max-h-60 border-2  my-2"
+                    className="lg:max-w-60 lg:max-h-60 md:max-w-32 md:max-h-fit border-2  md:ml-[30%] my-2"
                   />
                 ))}
               </div>
