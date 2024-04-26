730e062ac4031e0537d4b158d731bf8ccd6008de
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-26 13:22:35 +0200

    Update WhatWeDo.jsx

--- tailwind-app/src/components/WhatWeDo.jsx
+++ tailwind-app/src/components/WhatWeDo.jsx
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const WhatWeDo = () => {
   return (
-    <div className="max-w-[100%] lg:h-[30svh] md:h-[80svh] mt-20">
+    <div className="max-w-[100%] lg:h-[30svh] md:h-[50svh] mt-20">
       <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-10">
         <div className="mx-auto border-2 m-10 lg:w-[20svw] md:w-[20svh] sm:w-[40svw] h-auto p-5 flex flex-col items-center hover:scale-110 transition-all delay-75 cursor-pointer">
           <FontAwesomeIcon className="text-babyblue2" icon={faCat} size="4x" />
