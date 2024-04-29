f292037bd2c93c4281b590d9a5a5504e4bb8dcd5
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-29 11:42:07 +0200

    added bars

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import { faCat } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { faCat, faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
   return (
     <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white ">
       <div className="pl-16 text-2xl text-gray-400 hover:text-white hover:scale-110 transition-all delay-75 cursor-pointer">
@@ -10,6 +14,7 @@ export const Navbar = () => {
         <FontAwesomeIcon className="icon" icon={faCat} />
       </div>
       <div className="flex space-x-6 pr-16 text-gray-400 text-xl cursor-pointer">
+        <FontAwesomeIcon className="icon" icon={faBars} />
         <a href="/" className="hover:text-white">
           Home
         </a>
