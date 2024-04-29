86692ccb19c5c88d664698aee4a0553a6d357051
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-29 11:47:59 +0200

    Update Navbar.jsx

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
-import { faCat, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCat, faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
+
   const handleClick = () => {
     setOpen(!open);
   };
+
   return (
     <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white ">
       <div className="pl-16 text-2xl text-gray-400 hover:text-white hover:scale-110 transition-all delay-75 cursor-pointer">
         LITTLE PAWS
         <FontAwesomeIcon className="icon" icon={faCat} />
       </div>
-      <div className="flex space-x-6 pr-16 text-gray-400 text-xl cursor-pointer">
-        <FontAwesomeIcon className="icon" icon={faBars} />
+      <div className="flex space-x-6 pr-16 text-gray-400 text-xl cursor-pointer md:hidden">
+        {!open ? (
+          <FontAwesomeIcon className="" icon={faBars} onClick={handleClick} />
+        ) : (
+          <FontAwesomeIcon className="icon" icon={faX} onClick={handleClick} />
+        )}
+      </div>
+      <div
+        className={`${
+          open
+            ? "flex flex-col border border-white text-2xl p-6 mt-20"
+            : "hidden"
+        } md:pr-16 text-gray-400 md:text-xl cursor-pointer`}
+      >
         <a href="/" className="hover:text-white">
           Home
         </a>
