50ce6ceb17fc34ab85f98583683b7daedee835b3
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-04-29 13:27:02 +0200

    fixed navbar

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -10,45 +10,41 @@ export const Navbar = () => {
   };
 
   return (
-    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white ">
+    <div className="w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white">
       <div className="pl-16 text-2xl text-gray-400 hover:text-white hover:scale-110 transition-all delay-75 cursor-pointer">
         LITTLE PAWS
         <FontAwesomeIcon className="icon" icon={faCat} />
       </div>
-      <div className="flex space-x-6 pr-16 text-gray-400 text-xl cursor-pointer md:hidden">
-        {!open ? (
-          <FontAwesomeIcon className="" icon={faBars} onClick={handleClick} />
-        ) : (
-          <FontAwesomeIcon
-            className="icon"
-            icon={faTimes}
-            onClick={handleClick}
-          />
-        )}
+      <div className="md:hidden">
+        <button onClick={handleClick} className="text-xl text-gray-400  mr-4">
+          {open ? (
+            <FontAwesomeIcon icon={faTimes} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </button>
       </div>
-      <div
-        className={`absolute top-full right-0 ${
-          open
-            ? "flex flex-col border border-white text-2xl p-6 mr-16 absolute md:relative md:border-none"
-            : "hidden"
-        } md:pr-16 text-gray-400 md:text-xl cursor-pointer`}
+      <ul
+        className={`md:flex ${
+          open ? "block" : "hidden"
+        } font-mono md:mt-0 mt-4 absolute md:relative top-full right-0 md:top-0 md:right-auto md:flex flex-col md:flex-row bg-hero2 md:bg-transparent w-full md:w-auto justify-center items-center`}
       >
-        <a href="/" className="hover:text-white">
-          Home
-        </a>
-        <a href="#purpose" className="hover:text-white">
-          Purpose
-        </a>
-        <a href="#info" className="hover:text-white">
-          Info
-        </a>
-        <a href="#gallery" className="hover:text-white">
-          Gallery
-        </a>
-        <a href="#contact" className="hover:text-white">
-          Contact
-        </a>
-      </div>
+        <li className="p-4 hover:text-white" onClick={handleClick}>
+          <a href="/">Home</a>
+        </li>
+        <li className="p-4 hover:text-white" onClick={handleClick}>
+          <a href="#purpose">Purpose</a>
+        </li>
+        <li className="p-4 hover:text-white" onClick={handleClick}>
+          <a href="#info">Info</a>
+        </li>
+        <li className="p-4 hover:text-white" onClick={handleClick}>
+          <a href="#gallery">Gallery</a>
+        </li>
+        <li className="p-4 hover:text-white" onClick={handleClick}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
     </div>
   );
 };
