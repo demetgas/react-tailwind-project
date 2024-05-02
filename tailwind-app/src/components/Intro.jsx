ccdeabb2530916a5b8e51eae5eab1db6b06ae5d7
Demet Gaş <77278951+demetgas@users.noreply.github.com>
2024-05-02 10:45:12 +0200

    res

--- tailwind-app/src/components/Intro.jsx
+++ tailwind-app/src/components/Intro.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export const Intro = () => {
   return (
-    <div className="max-w-[100svw] mx-auto h-[40svh] bg-hero mt-64  text-white flex justify-center items-center">
+    <div className="max-w-[100svw] mx-auto lg:h-[35svh] md:h-[45vw] sm:h-[50vw] h-[80vw] bg-hero lg:mt-[7vw] mt-[13vw]  text-white flex justify-center items-center">
       <div className="text-center">
-        <h1 className="lg:text-4xl md:text-2xl  text-[20px] font-bold ">
+        <h1 className="lg:text-4xl md:text-[25px]  text-[22px] font-bold ">
           Welcome to our cat community, where our furry friends are here to
           support you!
         </h1>
-        <p className="lg:text-2xl md:text-xl  mt-8 text-[17px]">
+        <p className="lg:text-2xl md:text-[20px]  mt-8 text-[19px]">
           Join us to share in the joy and wonder that these charming creatures
           bring to our lives.
         </p>
